44a0cd00c06bff4995d46b4b42c9cde09fa2239f
Sania0786 <saniaahmed7123@gmail.com>
2025-04-27 10:54:11 +0530

    Delete predict.py

--- predict.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-def predict_news(text):
-    model = joblib.load('app/model.pkl')
-    vectorizer = joblib.load('app/vectorizer.pkl')
-    transformed_text = vectorizer.transform([text])
-    prediction = model.predict(transformed_text)
-    return "Real" if prediction[0] == 1 else "Fake"
